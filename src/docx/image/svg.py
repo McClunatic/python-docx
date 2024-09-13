378310f0549867de3cd8b8eebba1c6309a26936d
Brian McClune <brian.mcclune@proton.me>
2024-09-13 12:21:52 -0400

    Address FIXME in SVG image support

--- src/docx/image/svg.py
+++ src/docx/image/svg.py
@@ -1,3 +1,5 @@
+import math
+import re
 import xml.etree.ElementTree as ET
 
 from .constants import MIME_TYPE
@@ -38,8 +40,38 @@ class Svg(BaseImageHeader):
         stream.seek(0)
         data = stream.read()
         root = ET.fromstring(data)
-        # FIXME: The width could be expressed as '4cm'
-        # See https://www.w3.org/TR/SVG11/struct.html#NewDocument
-        width = int(root.attrib["width"])
-        height = int(root.attrib["height"])
-        return width, height
+
+        width = root.attrib["width"]
+        height = root.attrib["height"]
+
+        # Treat px and pt the same: presume everywhere a 72 dpi device
+        dpi = 72
+        if pt_match := re.match(r"(\d+(?:\.\d+)?)\s*p[tx]", width):
+            width, = pt_match.groups(1)
+        elif mm_match := re.match(r"(d\+(?:\.\d+)?)\s*mm", width):
+            dpmm = dpi * 25.4  # mm / in
+            width, = mm_match.groups(1) * dpmm
+        elif cm_match := re.match(r"(d\+(?:\.\d+)?)\s*cm", width):
+            dpcm = dpi * 2.54  # cm / in
+            width, = cm_match.groups(1) * dpcm
+        elif pc_match := re.match(r"(d\+(?:\.\d+)?)\s*pc", width):
+            dppc = dpi / 12  # 12 pt / pc
+            width, = pc_match.groups(1) * dppc
+        elif in_match := re.match(r"(d\+(?:\.\d+)?)\s*in", width):
+            width, = in_match.groups(1) * dpi
+
+        if pt_match := re.match(r"(\d+(?:\.\d+)?)\s*p[tx]", height):
+            height, = pt_match.groups(1)
+        elif mm_match := re.match(r"(d\+(?:\.\d+)?)\s*mm", height):
+            dpmm = dpi * 25.4  # mm / in
+            height, = mm_match.groups(1) * dpmm
+        elif cm_match := re.match(r"(d\+(?:\.\d+)?)\s*cm", height):
+            dpcm = dpi * 2.54  # cm / in
+            height, = cm_match.groups(1) * dpcm
+        elif pc_match := re.match(r"(d\+(?:\.\d+)?)\s*pc", height):
+            dppc = dpi / 12  # 12 pt / pc
+            height, = pc_match.groups(1) * dppc
+        elif in_match := re.match(r"(d\+(?:\.\d+)?)\s*in", height):
+            height, = in_match.groups(1) * dpi
+
+        return math.floor(float(width)), math.floor(float(height))
